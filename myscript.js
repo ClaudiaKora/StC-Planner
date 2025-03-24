cef2bff2e2689febcf8740a824daab65d947de31
Claudia <claudia.kraxner@fh-vie.ac.at>
2025-03-24 14:18:56 +0100

    Update myscript.js
    Signed-off-by: Claudia <claudia.kraxner@fh-vie.ac.at>

--- myscript.js
+++ myscript.js
@@ -1,4 +1,32 @@
-import { todoItems } from './main/data.js';
+const todoItems = [
+ {
+       name: "Visit the Botanical Gardens",
+       category: "Tagesausflug",
+       status: "Geplant",
+       possibleOn: "2025-03-29 09:00, - , 2025-03-27 17:00",
+       plannedFor: "-",
+       geotag: "40.7812, -73.9665",
+       tags: "nature, outdoors, relaxing"
+       },
+     {
+        name: "Visit the Botanical Gardens2",
+        category: "Abendgestaltung",
+        status: "Idee",
+        possibleOn: "2025-03-29 09:00 - 17:00",
+        plannedFor: "-",
+        geotag: "40.7812, -73.9665",
+        tags: "nature, outdoors, relaxing"
+       },
+    {
+        name: "Dinner at Italian Restaurant",
+        category: "Größeres",
+        status: "Mach ma Fix",
+        possibleOn: "2025-03-26 19:00 - 22:00",
+        plannedFor: "19:30",
+        geotag: "40.7580, -73.9855",
+        tags: "social, food, work"
+    },
+];
 
   //Java Class as a template for all the todoItems - stored in data.js...(?)
    // public class todoItem {
