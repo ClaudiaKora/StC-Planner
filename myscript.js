bee93c7a9052abf9e0ce44f4dee7041373c90502
Claudia <claudia.kraxner@fh-vie.ac.at>
2025-03-24 13:30:27 +0100

    Update myscript.js
    Signed-off-by: Claudia <claudia.kraxner@fh-vie.ac.at>

--- myscript.js
+++ myscript.js
@@ -1,28 +1,4 @@
-// User credentials
-const users = [
-    { username: "Claudia", password: "Kraxner" },
-    { username: "Stephan", password: "Ronge" }
-];
-
-function validateLogin() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    
-    const validUser = users.find(user => 
-        user.username === username && user.password === password
-    );
-    
-    if (validUser) {
-        document.getElementById("login-section").style.display = "none";
-        document.getElementById("items-section").style.display = "block";
-        alert("Welcome, " + username + "!");
-        
-        // Render the table with data
-        //renderTable(todoItems);
-    } else {
-        alert("Invalid username or password. Please try again.");
-    }
-}
+import { todoItems } from './data.js';
 
     
 //How Dropdown Fields Work
@@ -95,4 +71,32 @@ function renderTable(todoItems) {
     // Add remaining cells...
     tbody.appendChild(row); 
   });
+}
+
+// User credentials 
+const users = [
+    { username: "Claudia", password: "Kraxner" },
+    { username: "Stephan", password: "Ronge" }
+];
+
+
+//Login functionality
+function validateLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    const validUser = users.find(user => 
+        user.username === username && user.password === password
+    );
+    
+    if (validUser) {
+        document.getElementById("login-section").style.display = "none";
+        document.getElementById("items-section").style.display = "block";
+        alert("Welcome, " + username + "!");
+        
+        // Render the table with data
+        //renderTable(todoItems);
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
 }
