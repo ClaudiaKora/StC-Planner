30cbba918f1a0ea1fc48525bfedd2841ebd2f19d
Claudia <claudia.kraxner@fh-vie.ac.at>
2025-03-24 14:06:53 +0100

    Update data.js
    Signed-off-by: Claudia <claudia.kraxner@fh-vie.ac.at>

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 export const todoItems = [
  {
        name: "Visit the Botanical Gardens",
-       category: "weekend",
-       status: "idea",
+       category: "Tagesausflug",
+       status: "Geplant",
        possibleOn: "2025-03-29 09:00, - , 2025-03-27 17:00",
        plannedFor:  "void",
        geotag: "40.7812, -73.9665",
        tags: "nature, outdoors, relaxing"
        },
      {
-        name: "Visit the Botanical Gardens",
-        category: "weekend",
-        status: "idea",
+        name: "Visit the Botanical Gardens2",
+        category: "Abendgestaltung",
+        status: "Idee",
         possibleOn: "2025-03-29 09:00 - 17:00",
         plannedFor: "-",
         geotag: "40.7812, -73.9665",
@@ -19,8 +19,8 @@ export const todoItems = [
        },
     {
         name: "Dinner at Italian Restaurant",
-        category: "evening",
-        status: "confirmed",
+        category: "Größeres",
+        status: "Mach ma Fix",
         possibleOn: "2025-03-26 19:00 - 22:00",
         plannedFor: "19:30",
         geotag: "40.7580, -73.9855",
